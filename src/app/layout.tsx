b7af5215adae760d1973bb725e46a0c115fee7b7
Aman Singh <amansinghs7010@gmail.com>
2025-07-25 14:13:08 -0300

    embed chat widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -39,6 +41,15 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <div id="ixhello-root"></div>
+      <Script
+        id="ixhello-chat-widget"
+        src="https://d3at15qgbg1tx6.cloudfront.net/chat-widget/assets/chat-widget.js"
+        strategy="afterInteractive"
+        data-org-id="095cf8eb-b79d-4542-9ae1-dbd9cce8ed28"
+        data-chat-widget-id="f6396a19-d724-4d50-a177-75c5a74b5282"
+        data-flow-id="99abb682-565f-489a-b40f-35d916060d98"
+      />
     </html>
   );
 }
